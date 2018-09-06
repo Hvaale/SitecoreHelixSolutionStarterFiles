deddc51eb8f0c9ac8c80bf92f14b0e2e30951fcd
Ole Kristian Hvaale <Ole.kristian@hvaale.net>
2018-09-06 12:02:49 +0200

    Added simpe module-template for feature
    Added simpe module-template for feature

---
+++ module-template-feature/_Name_/code/Controllers/_Name_Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+
+namespace _NamespacePrefix_._ModuleType_._Name_.Controllers
+{
+  public class _Name_Controller : Controller
+  {
+
+    public ActionResult _Name_()
+    {
+       
+	   return View();
+   
+    }
+  }
+}
---
+++ module-template-feature/_Name_/code/Models/_Name_.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace _NamespacePrefix_._ModuleType_._Name_.Models
+{
+  public class _Name_ : 
+  {
+    // TODO: Implement model or otherwise remove this file
+  }
+}
